fbd3a8d5fbf7cb448d2fe2c8236681aef953baba
markedwardmurray <markedwardmurray@gmail.com>
2015-11-09 15:18:39 -0500

    pods: init with Quick & Nimble

---
+++ Pods/Target Support Files/Pods-swift-wwdc-badges-collectionsTests/Pods-swift-wwdc-badges-collectionsTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_swift_wwdc_badges_collectionsTests : NSObject
+@end
+@implementation PodsDummy_Pods_swift_wwdc_badges_collectionsTests
+@end
---
+++ Pods/Target Support Files/Pods-swift-wwdc-badges-collectionsTests/Pods-swift-wwdc-badges-collectionsTests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_swift_wwdc_badges_collectionsTestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_swift_wwdc_badges_collectionsTestsVersionString[];
+
