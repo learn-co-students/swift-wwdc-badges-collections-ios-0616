8d938becd157701712ea3dee80e65b985df150ce
MadinaIbrahim <madina.ibrahim.us@gmail.com>
2016-10-13 20:25:51 -0400

    Corrections

--- swift-wwdc-badges-collections/AppDelegate.swift
+++ swift-wwdc-badges-collections/AppDelegate.swift
@@ -7,17 +7,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-    var sand = SandBox()
+    
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        
+        let sand = SandBox()
+
         sand.badgeForSpeaker("Anita Borg")
                 
         sand.badgesForSpeakers(["Anita Borg", "Alan Kay", "Ada Lovelace", "Aaron Swartz", "Alan Turing", "Michael Faraday", "Grace Hopper"])
         
         sand.roomAssignmentsForSpeakers(["Anita Borg", "Alan Kay", "Ada Lovelace", "Aaron Swartz", "Alan Turing", "Michael Faraday", "Grace Hopper"])
         
-        sand.roomAssignmentsForSpeakers(["Anita Borg", "Alan Kay", "Ada Lovelace", "Aaron Swartz", "Alan Turing", "Michael Faraday", "Grace Hopper"])
-        
+        sand.roomAssignmentsForSpeakers(["Anita Borg", "Alan Kay", "Ada Lovelace", "Aaron Swartz", "Alan Turing", "Michael Faraday", "Grace Hopper", "Madina"])
+        sand.getRoomCapacitiesForAllSpeakers()
+        sand.audienceSizesForSpeakers(["Anita Borg", "Alan Kay", "Ada Lovelace", "Aaron Swartz", "Alan Turing", "Michael Faraday", "Grace Hopper", "Madina"])
         
         return true
     }
--- swift-wwdc-badges-collections/Sandbox.swift
+++ swift-wwdc-badges-collections/Sandbox.swift
@@ -2,24 +2,23 @@
 
 import Foundation
 
-class SandBox{
-    var speakersRoomsTable: [String:Int]? = ["Anita Borg":201,
-                                             "Alan Kay":202,
-                                             "Ada Lovelace":203,
-                                             "Aaron Swartz":215,
-                                             "Alan Turing":204,
-                                             "Michael Faraday":205,
-                                             "Grace Hopper":230,
-                                             "" : 245]
+class SandBox {
+    var speakersRoomsTable: [String:Int] = ["Anita Borg":201,
+                                            "Alan Kay":202,
+                                            "Ada Lovelace":203,
+                                            "Aaron Swartz":215,
+                                            "Alan Turing":204,
+                                            "Michael Faraday":205,
+                                            "Grace Hopper":230
+    ] // it's optional because IT DEPENS ON EXISTANCE OF THE KEY
+    var roomCapacitiesTable: [Int:Int] = [201:40, 202:50, 203:70, 215:30, 230:80, 240:35, 260:60]
     
     
     func badgeForSpeaker(name: String) -> String {
-        
         return "Hello, my name is \(name)"
     }
     
     func badgesForSpeakers(speakers: [String])-> [String] {
-        
         var badgesArray = [String] ()
         for name in speakers {
             let message = "Hello, my name is \(name)"
@@ -29,20 +28,46 @@ class SandBox{
     }
     
     func roomAssignmentsForSpeakers(speaker:[String])-> [String]{
-        
         let roomAssignments = [String]()
-        
         for person in speaker
         {
-            let room = speakersRoomsTable![person]
-    
-                if let roomNumber = room {
+            let room = speakersRoomsTable[person]
+            if let roomNumber = room {
                 print("\(person) will be speaking in Room \(roomNumber).")
-                } else {
+            } else {
                 print("\(person) has not been assigned to a room.")
             }
         }
         return roomAssignments
+    }
+    
+    func getRoomCapacitiesForAllSpeakers() -> [String:Int] {
+        var roomCapacitiesForSpeakers = [String:Int]()
+        
+        for (speaker,room) in speakersRoomsTable {
+            if let capacity = roomCapacitiesTable[room] {
+                roomCapacitiesForSpeakers[speaker] = capacity
+            }
+        }
+        print(roomCapacitiesForSpeakers)
+        return roomCapacitiesForSpeakers
+    }
+    
+    func audienceSizesForSpeakers(speakers:[String]) -> [String] {
+        let roomCapacities = getRoomCapacitiesForAllSpeakers()
+        
+        var audienceSize = [String]()
+        for eachSpeaker in speakers {
+            if let capacity = roomCapacities[eachSpeaker] {
+                let message = "\(eachSpeaker) can speak to up to \(capacity) people."
+                audienceSize.append(message)
+            } else {
+                let errorMessage = "Room information not found for \(eachSpeaker)."
+                audienceSize.append(errorMessage)
+            }
+        }
+        print(audienceSize)
+        return audienceSize
     }
 }
 
